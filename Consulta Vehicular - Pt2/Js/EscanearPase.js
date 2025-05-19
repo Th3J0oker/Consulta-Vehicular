2281e0841f938323288bc082865dce4b388b6696
Oscar Nuñez Trelles <108844489+Th3J0oker@users.noreply.github.com>
2025-05-19 12:08:31 -0500

    Delete Consulta Vehicular - Pt2 directory

--- Consulta Vehicular - Pt2/Js/EscanearPase.js
+++
@@ -1,9 +0,0 @@
-function abrirModalEscaner() {
-    document.getElementById('modalEscaner').classList.remove('hidden');
-    document.getElementById('modalEscaner').classList.add('flex');
-}
-
-function cerrarModalEscaner() {
-    document.getElementById('modalEscaner').classList.remove('flex');
-    document.getElementById('modalEscaner').classList.add('hidden');
-}
